b43927033fad1db8f4a1bc2d3bedb33a80a771b7
Henry Kupty <hkupty@gmail.com>
2023-02-24 20:07:12 +0100

    chore: Add a playground project
    This allows us to test the published maple version in maven

---
+++ playground/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+
+        logger.info("Hello world!");
+
+    }
+}
