f6a8bf9e9ca448167e5f40489993a30e3504a29b
Henry Kupty <hkupty@gmail.com>
2023-02-28 00:39:17 +0100

    test: Add new tests for TreeCache

--- maple-core/src/test/java/maple/core/logger/TreeCacheTests.java
+++ maple-core/src/test/java/maple/core/logger/TreeCacheTests.java
@@ -1,103 +1,159 @@
-package maple.api.manager;
+package maple.core.logger;
 
-import maple.core.logger.TreeCache;
-import maple.core.logger.guard.WarnLevelGuard;
-import maple.core.logger.guard.InfoLevelGuard;
-import maple.core.slf4j.MapleLoggerFactory;
+import maple.api.config.Config;
+import maple.core.logger.guard.DebugLevelGuard;
 import org.junit.jupiter.api.Test;
 import org.slf4j.event.Level;
 
-import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class TreeCacheTests {
 
-//    @Test
-//    void canGetRootLogger() {
-//        var cache = new TreeCache(new TestLogger(""));
-//        assertNotNull(cache.find(new String[]{}));
-//    }
-//
-//    @Test
-//    void canCreateAChildLogger() {
-//        var cache = new TreeCache(new TestLogger(""));
-//        cache.createRecursively(new String[]{"com", "github", "hkupty", "maple", "LeLogger"},
-//                (baseLogger, partialIdentifier) ->
-//                        new TestLogger(String.join(".", partialIdentifier))
-//        );
-//        assertNotNull(cache.find(new String[]{}));
-//        assertNotNull(cache.find(new String[]{"com"}));
-//        assertNotNull(cache.find(new String[]{"com", "github"}));
-//        assertNotNull(cache.find(new String[]{"com", "github", "hkupty"}));
-//        assertNotNull(cache.find(new String[]{"com", "github", "hkupty", "maple"}));
-//        var finalLogger = cache.find(new String[]{"com", "github", "hkupty", "maple", "LeLogger"});
-//        assertNotNull(finalLogger);
-//        assertEquals(InfoLevelGuard.singleton(), ((TestLogger)finalLogger).getEventBuilderFactory());
-//    }
-//
-//    @Test
-//    void canRecursivelyUpdateChildren() {
-//        var cache = new TreeCache(new TestLogger(""));
-//        cache.createRecursively(new String[]{"com", "github", "hkupty", "maple", "LeLogger"},
-//                (baseLogger, partialIdentifier) ->
-//                        new TestLogger(String.join(".", partialIdentifier))
-//        );
-//        cache.updateConfig(new String[]{"com", "github"}, old -> old.copy(Level.WARN));
-//
-//        var nodeLogger = cache.find(new String[]{"com"});
-//        assertNotNull(nodeLogger);
-//        assertEquals(InfoLevelGuard.singleton(), ((TestLogger)nodeLogger).getEventBuilderFactory());
-//
-//        var finalLogger = cache.find(new String[]{"com", "github", "hkupty", "maple", "LeLogger"});
-//        assertNotNull(finalLogger);
-//        assertEquals(WarnLevelGuard.singleton(), ((TestLogger)finalLogger).getEventBuilderFactory());
-//    }
-//    @Test
-//    void canRecursivelyUpdateAllChildren() {
-//        var cache = new TreeCache(new TestLogger(""));
-//        cache.createRecursively(new String[]{"com", "github", "hkupty", "maple", "LeLogger"},
-//                (baseLogger, partialIdentifier) ->
-//                        new TestLogger(String.join(".", partialIdentifier))
-//        );
-//        cache.updateConfig(new String[]{}, old -> old.copy(Level.WARN));
-//
-//        var rootLogger = cache.find(new String[]{"com"});
-//        assertNotNull(rootLogger);
-//        assertEquals(WarnLevelGuard.singleton(), ((TestLogger)rootLogger).getEventBuilderFactory());
-//
-//        var nodeLogger = cache.find(new String[]{"com"});
-//        assertNotNull(nodeLogger);
-//        assertEquals(WarnLevelGuard.singleton(), ((TestLogger)nodeLogger).getEventBuilderFactory());
-//
-//        var finalLogger = cache.find(new String[]{"com", "github", "hkupty", "maple", "LeLogger"});
-//        assertNotNull(finalLogger);
-//        assertEquals(WarnLevelGuard.singleton(), ((TestLogger)finalLogger).getEventBuilderFactory());
-//    }
+    @Test
+    public void empty_cache_has_depth_1(){
+        var config = Config.getDefault();
+        var cache = new TreeCache(config);
+        AtomicInteger depth = new AtomicInteger();
+        cache.traverse(cache.ROOT, ignored -> depth.getAndIncrement());
+
+        assertEquals(1, depth.get());
+    }
 
     @Test
-    void testLog() {
-        var logger = MapleLoggerFactory.getInstance().getLogger("test");
-        var nested = new HashMap<>();
-        nested.put("nested", "map");
-        var map = new HashMap<>();
-        map.put("Something", nested);
-        map.put("other", new Object[]{
-                "thing"
+    public void empty_cache_has_no_loggers_created(){
+        var config = Config.getDefault();
+        var cache = new TreeCache(config);
+        AtomicInteger depth = new AtomicInteger();
+        cache.traverse(cache.ROOT, data -> {
+            if(data.logger != null) {
+                depth.getAndIncrement();
+            }
         });
-        logger.atInfo()
-                .addKeyValue("something", map)
-                .addKeyValue("directly", new Object[]{
-                        "here",
-                        1,
-                        2.3
-                })
-                .log("stuff");
-        logger.atInfo().log("stuff");
-        logger.atInfo().log("stuff");
-        logger.atInfo().log("stuff");
-        logger.atInfo().log("stuff");
+
+        assertEquals(0, depth.get());
+    }
+
+
+    @Test
+    public void requesting_a_logger_from_cache_creates_entries_recursively(){
+        var config = Config.getDefault();
+        var cache = new TreeCache(config);
+        cache.getOrCreate(new String[]{
+                "com",
+                "for",
+                "testing"
+        });
+        AtomicInteger depth = new AtomicInteger();
+        cache.traverse(cache.ROOT, ignored -> depth.getAndIncrement());
+
+        assertEquals(4, depth.get());
+    }
+
+    @Test
+    public void requesting_a_logger_from_cache_creates_only_one_logger(){
+        var config = Config.getDefault();
+        var cache = new TreeCache(config);
+        cache.getLoggerAt(new String[]{
+                "com",
+                "for",
+                "testing"
+        });
+        AtomicInteger depth = new AtomicInteger();
+        cache.traverse(cache.ROOT, data -> {
+            if(data.logger != null) {
+                depth.getAndIncrement();
+            }
+        });
+
+        assertEquals(1, depth.get());
+    }
+    @Test
+    public void calling_getOrCreate_twice_does_not_create_duplicates(){
+        var config = Config.getDefault();
+        var cache = new TreeCache(config);
+
+        cache.getOrCreate(new String[]{
+                "com",
+                "for",
+                "testing"
+        });
+
+        cache.getOrCreate(new String[]{
+                "com",
+                "for",
+                "testing"
+        });
+
+        AtomicInteger depth = new AtomicInteger();
+        cache.traverse(cache.ROOT, ignored -> depth.getAndIncrement());
+
+        assertEquals(4, depth.get());
+
+        AtomicInteger loggers = new AtomicInteger();
+        cache.traverse(cache.ROOT, data -> {
+            if(data.logger != null) {
+                loggers.getAndIncrement();
+            }
+        });
+
+        assertEquals(0, loggers.get());
+    }
+
+    @Test
+    public void updating_uninitialized_parent_cascades_down_to_materialized_logger(){
+        var config = Config.getDefault();
+        var cache = new TreeCache(config);
+        var logger = cache.getLoggerAt(new String[]{
+                "com",
+                "for",
+                "testing"
+        });
+        assertEquals(Level.INFO, logger.getConfig().level());
+        cache.updateConfig(new String[]{"com", "for"}, cfg -> cfg.copy(Level.DEBUG));
+        assertEquals(Level.DEBUG, logger.getConfig().level());
+        assertEquals(DebugLevelGuard.singleton(), logger.levelGuard);
+    }
+
+    @Test
+    public void updating_uninitialized_parent_does_not_materialize_logger(){
+        var config = Config.getDefault();
+        var cache = new TreeCache(config);
+        cache.getLoggerAt(new String[]{
+                "com",
+                "for",
+                "testing"
+        });
+        cache.updateConfig(new String[]{"com", "for"}, cfg -> cfg.copy(Level.DEBUG));
+        AtomicInteger loggers = new AtomicInteger();
+        cache.traverse(cache.ROOT, data -> {
+            if(data.logger != null) {
+                loggers.getAndIncrement();
+            }
+        });
+
+        assertEquals(1, loggers.get());
+    }
+
+    @Test
+    public void updating_uninitialized_config_will_recursively_create_the_entries_but_not_loggers(){
+        var config = Config.getDefault();
+        var cache = new TreeCache(config);
+        cache.updateConfig(new String[]{"com", "for", "testing"}, cfg -> cfg.copy(Level.DEBUG));
+        AtomicInteger depth = new AtomicInteger();
+        cache.traverse(cache.ROOT, ignored -> depth.getAndIncrement());
+
+        assertEquals(4, depth.get());
+
+        AtomicInteger loggers = new AtomicInteger();
+        cache.traverse(cache.ROOT, data -> {
+            if(data.logger != null) {
+                loggers.getAndIncrement();
+            }
+        });
+
+        assertEquals(0, loggers.get());
     }
 
 }
