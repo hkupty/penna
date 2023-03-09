85d9c982e84583692f28b4794616ee4eeef717cc
Henry Kupty <hkupty@gmail.com>
2023-03-09 13:20:01 +0100

    refactor: Move the precision to an external variable
    In the future, it can even be that we might want to tweak this value.
    If we make it smaller, it'll be more precise at the expense of some CPU processing;
    If we make it bigger, it'll be less precise, but spare CPU;
    
    It is important to note that 1ms interval is not necessarily correct in terms of precision given, in linux,
    System.currentTimeMillis() takes several hundred useconds.

--- maple-core/src/main/java/maple/core/internals/Clock.java
+++ maple-core/src/main/java/maple/core/internals/Clock.java
@@ -6,12 +6,16 @@ import java.util.concurrent.locks.LockSupport;
 public final class Clock {
     private Clock() {}
 
+    // TODO make precision configurable;
+    //  i.e. allow the clock to be updated every second, to reduce the load.
+    //  Also, it is not necessarily precise already because it takes half a ms to update.
+    private static final long precision = 1_000_000;
 
     private static final AtomicLong timestamp = new AtomicLong(0L);
     private static final Thread clockThread = ThreadCreator.newThread("maple-clock-ticker", () -> {
         while(true) {
             timestamp.set(System.currentTimeMillis());
-            LockSupport.parkNanos(1_000_000);
+            LockSupport.parkNanos(precision);
         }
     });
 
