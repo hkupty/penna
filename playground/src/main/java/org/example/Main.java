fa57682f5a40ef6678c3544f79dfbd52bfe43769
Henry Kupty <hkupty@gmail.com>
2023-03-10 15:03:26 +0100

    chore: Just stabilize

--- playground/src/main/java/org/example/Main.java
+++ playground/src/main/java/org/example/Main.java
@@ -6,8 +6,12 @@ import org.slf4j.LoggerFactory;
 public class Main {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(Main.class);
+        Logger otherLogger = LoggerFactory.getLogger("simple.logger");
 
         logger.info("Hello world!");
+        logger.debug("This should be shown");
+        otherLogger.info("Hello world!");
+        otherLogger.debug("This should be not shown");
 
     }
 }
