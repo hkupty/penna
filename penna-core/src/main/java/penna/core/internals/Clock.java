35f9cc45a0d1aaa3314865c333ce6c028cddc6ff
Henry Kupty <hkupty@gmail.com>
2023-03-11 10:05:50 +0100

    style: address sonarcloud issues

--- penna-core/src/main/java/penna/core/internals/Clock.java
+++ penna-core/src/main/java/penna/core/internals/Clock.java
@@ -9,11 +9,11 @@ public final class Clock {
     private Clock() {}
 
     // This is the amount of time, in nanos, that we will wait before incrementing.
-    // Keeping this at 1_000_000 ensure we tick at roughly 1ms;
-    private static final long refreshRate = 1_000_000;
+    // Keeping this at 1 million ensure we tick at roughly 1ms;
+    private static final long REFRESH_RATE = 1_000_000;
 
-    // The precision is how far behind, in microseconds, we afford to be before we sync to System.currentTimeMillis
-    private static final long precision = 1_000;
+    // This is how often, in ms, we should sync with System.currentTimeMillis();
+    private static final long PRECISION = 1_000;
     private static final Lock startThreadLock = new ReentrantLock();
 
     private static final AtomicLong timestamp = new AtomicLong(System.currentTimeMillis());
@@ -23,10 +23,10 @@ public final class Clock {
 
             // System.currentTimeMillis is known to be slow on linux.
             // We can increment manually and sync at every second maybe?
-            if (ts % precision == 0) {
+            if (ts % PRECISION == 0) {
                 timestamp.set(System.currentTimeMillis());
             }
-            LockSupport.parkNanos(refreshRate);
+            LockSupport.parkNanos(REFRESH_RATE);
         }
     });
 
