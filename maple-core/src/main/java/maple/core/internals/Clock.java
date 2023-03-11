34d2efa817d9225a14c5a16f1054f6875c01a75b
Henry Kupty <hkupty@gmail.com>
2023-03-11 09:53:20 +0100

    refactor: Avoid doing currentTimeMillis too often
    That method takes a few hundred usecs in linux, which will cause the logger to be out of sync
    frequently. By increasing the counter manually we might get a slightly more confident result
    actually. But we sync back to System.currentTimeMillis at every thousand iterations (roughly
    a second) to make sure we don't drift apart too much

--- maple-core/src/main/java/maple/core/internals/Clock.java
+++ maple-core/src/main/java/maple/core/internals/Clock.java
@@ -8,17 +8,25 @@ import java.util.concurrent.locks.ReentrantLock;
 public final class Clock {
     private Clock() {}
 
-    // TODO make precision configurable;
-    //  i.e. allow the clock to be updated every second, to reduce the load.
-    //  Also, it is not necessarily precise already because it takes half a ms to update.
-    private static final long precision = 1_000_000;
-    private static Lock startThreadLock = new ReentrantLock();
+    // This is the amount of time, in nanos, that we will wait before incrementing.
+    // Keeping this at 1_000_000 ensure we tick at roughly 1ms;
+    private static final long refreshRate = 1_000_000;
 
-    private static final AtomicLong timestamp = new AtomicLong(0L);
+    // The precision is how far behind, in microseconds, we afford to be before we sync to System.currentTimeMillis
+    private static final long precision = 1_000;
+    private static final Lock startThreadLock = new ReentrantLock();
+
+    private static final AtomicLong timestamp = new AtomicLong(System.currentTimeMillis());
     private static final Thread clockThread = ThreadCreator.newThread("maple-clock-ticker", () -> {
         while(true) {
-            timestamp.set(System.currentTimeMillis());
-            LockSupport.parkNanos(precision);
+            var ts = timestamp.incrementAndGet();
+
+            // System.currentTimeMillis is known to be slow on linux.
+            // We can increment manually and sync at every second maybe?
+            if (ts % precision == 0) {
+                timestamp.set(System.currentTimeMillis());
+            }
+            LockSupport.parkNanos(refreshRate);
         }
     });
 
