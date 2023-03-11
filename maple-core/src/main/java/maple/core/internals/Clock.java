0c41ef9f2119fa2656a5a25ad955cea30e4be9e8
Henry Kupty <hkupty@gmail.com>
2023-03-11 09:16:18 +0100

    refactor: Simplify thread init logic
    Using a lock instead of synchronized allows us to simplify the logic by skipping the second check.
    While this is only useful during thread initialization, it is vital to avoid race conditions.
    
    The alternative would be to always start the thread, which might be excessive if we're not using
    timestamp

--- maple-core/src/main/java/maple/core/internals/Clock.java
+++ maple-core/src/main/java/maple/core/internals/Clock.java
@@ -1,7 +1,9 @@
 package maple.core.internals;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.LockSupport;
+import java.util.concurrent.locks.ReentrantLock;
 
 public final class Clock {
     private Clock() {}
@@ -10,6 +12,7 @@ public final class Clock {
     //  i.e. allow the clock to be updated every second, to reduce the load.
     //  Also, it is not necessarily precise already because it takes half a ms to update.
     private static final long precision = 1_000_000;
+    private static Lock startThreadLock = new ReentrantLock();
 
     private static final AtomicLong timestamp = new AtomicLong(0L);
     private static final Thread clockThread = ThreadCreator.newThread("maple-clock-ticker", () -> {
@@ -20,14 +23,11 @@ public final class Clock {
     });
 
     public static long getTimestamp() {
-        if (!clockThread.isAlive()) {
-            synchronized (clockThread) {
-                if (!clockThread.isAlive()) {
-                    // We preload the current value just in case we return before the thread updates
-                    timestamp.updateAndGet(ignored -> System.currentTimeMillis());
-                    clockThread.start();
-                }
-            }
+        if (!clockThread.isAlive() && startThreadLock.tryLock()) {
+            // We preload the current value just in case we return before the thread updates
+            timestamp.updateAndGet(ignored -> System.currentTimeMillis());
+            clockThread.start();
+            startThreadLock.unlock();
         }
 
         return timestamp.get();
