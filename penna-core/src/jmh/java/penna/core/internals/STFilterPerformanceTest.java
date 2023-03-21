b25ea01f75d697b4ec65760b0a7ab15f65a2ea67
Henry Kupty <hkupty@gmail.com>
2023-03-21 10:53:09 +0100

    test(jmh): Add performance tests

---
+++ penna-core/src/jmh/java/penna/core/internals/STFilterPerformanceTest.java
@@ -0,0 +1,104 @@
+package penna.core.internals;
+
+import org.junit.jupiter.api.Test;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+import penna.api.config.Config;
+import penna.core.logger.PennaLogger;
+import penna.core.logger.SinkPerformanceTest;
+import penna.core.logger.TreeCache;
+import penna.core.sink.SinkImpl;
+import penna.core.sink.impl.GsonPennaSink;
+import penna.core.sink.impl.JacksonPennaSink;
+import penna.core.sink.impl.JakartaPennaSink;
+import penna.core.sink.impl.NativePennaSink;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class STFilterPerformanceTest {
+
+    @State(Scope.Thread)
+    public static class SimpleState {
+        @Param({"16", "64", "128", "512"})
+        int size;
+
+        StackTraceElement[] stacktrace;
+
+        Random random;
+
+        @Setup
+        public void setup() {
+            random = new Random();
+            stacktrace = new StackTraceElement[size];
+            for (int i = 0; i < size; i++ ) {
+                stacktrace[i] = new StackTraceElement(
+                        STFilterPerformanceTest.SimpleState.class.getName(),
+                        "setup",
+                        "STFilterPerformanceTest.java",
+                        i);
+            }
+        }
+
+    }
+
+
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void markAndCheck(SimpleState state){
+        StackTraceFilter filter = StackTraceFilter.create();
+        StackTraceElement[] stackTrace = state.stacktrace;
+        for (int index = 0; index < stackTrace.length; index++) {
+            filter.mark(stackTrace[index]);
+        }
+
+        for (int index = 0; index < stackTrace.length; index++) {
+            if(filter.check(stackTrace[state.random.nextInt(state.size)])) break;
+        }
+    }
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void compareWithHashSet(SimpleState state){
+        Set<StackTraceElement> set = new HashSet<>();
+        StackTraceElement[] stackTrace = state.stacktrace;
+        for (int index = 0; index < stackTrace.length; index++) {
+            set.add(stackTrace[index]);
+        }
+
+        for (int index = 0; index < stackTrace.length; index++) {
+            if(set.contains(stackTrace[state.random.nextInt(state.size)])) break;
+        }
+    }
+
+
+    @Test
+    public void runBenchmarks() throws Exception {
+        Options options = new OptionsBuilder()
+                .include(this.getClass().getName() + ".*")
+                .mode(Mode.AverageTime)
+                .warmupTime(TimeValue.seconds(10))
+                .warmupIterations(3)
+                .threads(1)
+                .measurementIterations(3)
+                .forks(1)
+                .shouldFailOnError(true)
+                .shouldDoGC(true)
+                .addProfiler("gc")
+                .build();
+
+        new Runner(options).run();
+    }
+
+}
---
+++ penna-core/src/jmh/java/penna/core/logger/LoggerPerformanceTest.java
@@ -0,0 +1,180 @@
+package penna.core.logger;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.contrib.jackson.JacksonJsonFormatter;
+import ch.qos.logback.contrib.json.JsonFormatter;
+import ch.qos.logback.contrib.json.classic.JsonLayout;
+import ch.qos.logback.core.encoder.Encoder;
+import ch.qos.logback.core.encoder.LayoutWrappingEncoder;
+import net.logstash.logback.encoder.LogstashEncoder;
+import org.junit.jupiter.api.Test;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+import org.slf4j.MDC;
+import org.slf4j.MarkerFactory;
+import penna.api.config.Config;
+import penna.core.logger.utils.LogbackDevNullAppender;
+import penna.core.sink.SinkImpl;
+import penna.core.sink.impl.NativePennaSink;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+public class LoggerPerformanceTest {
+
+    @State(Scope.Thread)
+    public static class TestBehavior {
+        @Param({"simple", "modest", "moderate", "large"})
+        String behavior;
+
+        public void log(org.slf4j.Logger logger) {
+            switch (behavior) {
+                case "modest" -> {
+                    logger.atInfo()
+                            .addMarker(MarkerFactory.getMarker("For the win!"))
+                            .log("Some event");
+                }
+                case "moderate" -> {
+                    MDC.put("SomeKey", "some value");
+                    logger
+                            .atInfo()
+                            .addMarker(MarkerFactory.getMarker("For the win!"))
+                            .addArgument(UUID::randomUUID)
+                            .log("Some event: {}");
+                    MDC.remove("SomeKey");
+                }
+                case "large" -> {
+                    MDC.put("SomeKey", "some value");
+                    logger
+                            .atInfo()
+                            .addMarker(MarkerFactory.getMarker("For the win!"))
+                            .addArgument(UUID::randomUUID)
+                            .log("Some event: {}", new RuntimeException("with an exception"));
+                    MDC.remove("SomeKey");
+                }
+                default -> {
+                    logger.atInfo().log("hello world");
+
+                }
+            }
+
+        }
+    }
+
+    @State(Scope.Thread)
+    public static class LogbackState {
+        LoggerContext context = new LoggerContext();
+        Logger logger;
+        @Param({"contrib", "logstash"})
+        String encoder;
+
+        private Encoder<ILoggingEvent> getContribEncoder() {
+            var encoder = new LayoutWrappingEncoder<ILoggingEvent>();
+            var layout = new JsonLayout();
+            var formatter = new JacksonJsonFormatter();
+
+            encoder.setContext(context);
+            layout.setContext(context);
+
+            formatter.setPrettyPrint(false);
+            layout.setJsonFormatter(formatter);
+            layout.setAppendLineSeparator(true);
+            encoder.setLayout(layout);
+
+            return encoder;
+        }
+
+        private Encoder<ILoggingEvent> getLogstashEncoder() {
+            var encoder = new LogstashEncoder();
+            encoder.setContext(context);
+            encoder.setIncludeMdc(true);
+            encoder.setIncludeTags(true);
+
+            return encoder;
+        }
+
+        @Setup
+        public void setup() {
+            context.setName("JMH");
+            logger = context.getLogger("jmh.test.logback");
+            logger.setLevel(Level.INFO);
+            var appender = new LogbackDevNullAppender();
+            appender.setContext(context);
+
+            var encoder = switch (this.encoder) {
+                case "contrib" -> getContribEncoder();
+                case "logstash" -> getLogstashEncoder();
+                default -> throw new RuntimeException("Unable to match logger");
+            };
+
+            appender.setEncoder(encoder);
+            logger.addAppender(appender);
+
+            appender.start();
+        }
+
+    }
+
+    @State(Scope.Thread)
+    public static class PennaState {
+        private final File dumpToNull = new File("/dev/null");
+        private FileOutputStream fos;
+        TreeCache cache = new TreeCache(Config.getDefault());
+        PennaLogger logger;
+
+        @Setup
+        public void setup() throws IOException {
+            fos = new FileOutputStream(dumpToNull);
+            SinkImpl sink = new NativePennaSink();
+            sink.init(fos.getChannel());
+            logger = cache.getLoggerAt("jmh", "test", "penna");
+            logger.sink.set(sink);
+        }
+        @TearDown
+        public void tearDown() throws IOException {
+            fos.close();
+        }
+    }
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void penna(PennaState state, TestBehavior tb) throws IOException {
+        tb.log(state.logger);
+    }
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void logback(LogbackState state, TestBehavior tb) throws IOException {
+        tb.log(state.logger);
+    }
+
+
+    @Test
+    public void runBenchmarks() throws Exception {
+        Options options = new OptionsBuilder()
+                .include(this.getClass().getName() + ".*")
+                .mode(Mode.AverageTime)
+                .warmupTime(TimeValue.seconds(15))
+                .warmupIterations(3)
+                .threads(1)
+                .measurementIterations(3)
+                .forks(1)
+                .shouldFailOnError(true)
+                .shouldDoGC(true)
+                .addProfiler("gc")
+                .build();
+
+        new Runner(options).run();
+    }
+
+}
