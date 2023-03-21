ba7d49eff8231d5e75086ca95af432603fe6bda2
Henry Kupty <hkupty@gmail.com>
2023-03-21 18:19:50 +0100

    refactor: StackTraceFilter improvements
    There are a few worth noting here:
      - k has been reduced to 2, because 6 is overkill;
      - the hash algorithms have been adapted (to be tested for uniform
        distribution still)
      - The loop has been unrolled since position 0 is effectively h1 only
        while position 1 can pretty much be h2 only then;
      - Since the usage was check-then-mark always, it did not make sense to
        re-calculate the hashes twice so close together, so the positions
        array is exposed and expected to be passed in as argument for the
        respective `check` and `mark` functions, saving some time;
    
    Now, the API is a bit confusing and deserves some refactoring for
    maintainability, but the commit will go as-is to avoid piling up too
    much.

--- penna-core/src/main/java/penna/core/internals/StackTraceFilter.java
+++ penna-core/src/main/java/penna/core/internals/StackTraceFilter.java
@@ -2,48 +2,66 @@ package penna.core.internals;
 
 import java.util.BitSet;
 
-public record StackTraceFilter(BitSet bitSet) {
+public record StackTraceFilter(BitSet bitSet, int[] cursor) {
 
     private static final int FILTER_SIZE = 8 * 1024;
-    private static final int NUMBER_OF_HASHES = 6;
+    private static final int NUMBER_OF_HASHES = 2;
 
     private static final ThreadLocal<BitSet> BASE = ThreadLocal.withInitial(() -> new BitSet(FILTER_SIZE));
+
+    public StackTraceFilter reset() {
+        bitSet.clear();
+        return this;
+    }
 
     public static StackTraceFilter create() {
         var bitset = BASE.get();
         bitset.clear();
 
-        return new StackTraceFilter(bitset);
+        return new StackTraceFilter(bitset, new int[NUMBER_OF_HASHES]);
     }
 
-    private int[] hash(StackTraceElement element) {
-        int[] positions = new int[NUMBER_OF_HASHES];
-        int prime = 1048573; // a large prime number
+    public void hash(StackTraceElement element, int[] positions) {
+        int size = bitSet().size();
+        int prime = 1048573;
 
+        // 32 - 13. 2^13 = 8 * 1024 = FILTER_SIZE
+        // So hash2 will be definitely smaller than
         int hash1 = element.hashCode();
-        int hash2 = prime - (hash1 % prime);
+        int hash2 = ((hash1 * prime) & Integer.MAX_VALUE) >> 19;
 
-        for (int i = 0; i < NUMBER_OF_HASHES; i++) {
-            positions[i] = Math.abs((hash1 + i * hash2) % bitSet().size());
-        }
-        return positions;
+        // To avoid off-by-one errors
+        positions[0] = ((hash1 & Integer.MAX_VALUE)  >> 19) - 1;
+        positions[1] = hash2 - 1;
+
     }
 
+    public int[] hash(StackTraceElement element) {
+        hash(element, this.cursor);
+
+        return this.cursor;
+    }
 
     public void mark(StackTraceElement stackTraceElement) {
-        for (int position : hash(stackTraceElement)) {
-            bitSet.set(position, true);
+        mark(hash(stackTraceElement));
+    }
+
+    public void mark(int[] positions) {
+        for (int position : positions) {
+            bitSet.set(position);
         }
     }
 
     public boolean check(StackTraceElement stackTraceElement) {
-        boolean isSet = true;
-        for (int position : hash(stackTraceElement)) {
-            isSet &= bitSet.get(position);
-            if (!isSet) break;
+        return check(hash(stackTraceElement));
+    }
+
+    public boolean check(int[] positions) {
+        for (int position : positions) {
+            if (bitSet.get(position)) return true;
         }
 
-        return isSet;
+        return false;
     }
 
 }
