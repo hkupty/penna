d7d036c39198a57d28ed3130a0d544a91d6e02dd
Henry Kupty <hkupty@gmail.com>
2023-03-30 22:24:24 +0200

    refactor: Log in json from the minilogger

--- penna-core/src/main/java/penna/core/minilog/MiniLogger.java
+++ penna-core/src/main/java/penna/core/minilog/MiniLogger.java
@@ -5,25 +5,27 @@ package penna.core.minilog;
 public final class MiniLogger {
     private MiniLogger() {}
     public static void error(String message) {
-        System.err.print("[:Penna.MiniLogger/Error \"");
+        System.err.print("{\"logger\":\"penna.core.MiniLogger\",\"level\":\"ERROR\",\"message\":\"");
         System.err.print(message);
-        System.err.println("\"]");
+        System.err.println("\"}");
     }
 
     public static void error(String message, Throwable throwable) {
         error(message);
-        System.err.print("[:Penna.MiniLogger/Exception ");
+        System.err.print("{\"logger\":\"penna.core.MiniLogger\",\"level\":\"ERROR\",\"message\":\"");
         System.err.println(throwable.getMessage());
+        System.err.println("\", \"throwable\":\"");
         var stack = throwable.getStackTrace();
         for(int i = 0; i < stack.length; i++) {
             System.err.println(stack[i].toString());
+            System.err.println("\n");
         }
-        System.err.print("]");
+        System.err.println("\"}");
     }
 
     public static void debug(String message) {
-        System.err.print("[:Penna/MiniLogger/Debug \"");
+        System.err.print("{\"logger\":\"penna.core.MiniLogger\",\"level\":\"ERROR\",\"message\":\"");
         System.err.print(message);
-        System.err.println("\"]");
+        System.err.println("\"}");
     }
 }
