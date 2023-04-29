96845db256138f5465863b87208c92fb472a0f78
Henry Kupty <hkupty@gmail.com>
2023-04-29 12:07:34 +0200

    feat: Add exceptions config

--- penna-yaml-config/src/main/java/penna/config/yaml/models/Node.java
+++ penna-yaml-config/src/main/java/penna/config/yaml/models/Node.java
@@ -7,14 +7,31 @@ public sealed interface Node {
     String level();
     List<String> fields();
 
+    Exceptions exceptions();
+
+    sealed interface Exceptions {
+
+        boolean deduplicate();
+        int maxDepth();
+
+        record ExceptionsConfig(
+                boolean deduplicate,
+                int maxDepth
+        ) implements Exceptions {}
+    }
+
     record YamlConfigNode(
             String level,
-            List<String> fields
+            List<String> fields,
+
+            Exceptions exceptions
+
     ) implements Node {}
 
    record RootNode(
             String level,
             List<String> fields,
+            Exceptions exceptions,
             Map<String, YamlConfigNode> loggers
     ) implements Node {}
 
