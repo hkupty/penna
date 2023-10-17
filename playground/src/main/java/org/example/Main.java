43d76473833cf0f319e3a0bb411920a6a5905b59
Henry Kupty <hkupty@gmail.com>
2023-10-17 21:19:14 +0200

    test: Fix playground tests

--- playground/src/main/java/org/example/Main.java
+++ playground/src/main/java/org/example/Main.java
@@ -2,16 +2,24 @@ package org.example;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 public class Main {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(Main.class);
         Logger otherLogger = LoggerFactory.getLogger("simple.logger");
+        MDC.put("some", "data");
+        MDC.put("other", "data");
 
-        logger.info("Hello world!", new RuntimeException("Something awful!"));
+        logger.atInfo()
+                .addKeyValue("something", -43.2)
+                .log("Hello world!", new RuntimeException("Something awful!"));
         logger.debug("This should be shown");
         otherLogger.info("Hello world!");
         otherLogger.debug("This should be not shown");
+
+        MDC.remove("some");
+        MDC.remove("other");
 
     }
 }
