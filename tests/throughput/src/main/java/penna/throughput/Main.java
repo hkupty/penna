102685496358f62cba6a0b5eaf83a032f44c0518
Henry Kupty <hkupty@gmail.com>
2023-10-17 21:19:02 +0200

    tests: Simplify throughput test

--- tests/throughput/src/main/java/penna/throughput/Main.java
+++ tests/throughput/src/main/java/penna/throughput/Main.java
@@ -5,53 +5,51 @@ import org.slf4j.LoggerFactory;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.LockSupport;
+import java.util.stream.IntStream;
 
 public class Main {
+
     public static void main(String[] args) {
-
-        class Inner {
-            Exception data;
-            Exception getSuppressed() {
-                return new RuntimeException("WTH!!");
-            }
-            void setData() {
-                data = new IllegalArgumentException("wrong '\"type!!!\nok???", new RuntimeException("inner", new ArrayIndexOutOfBoundsException(5)));
-                data.addSuppressed(getSuppressed());
-            }
-
-            Exception getData() {
-                setData();
-                return data;
-            }
-        }
-        int time = 10;
+        int time;
         int threads = 1;
         if (args.length > 0) {
             time = Integer.parseInt(args[0]);
+        } else {
+            time = 10;
         }
         if (args.length > 1) {
             threads = Integer.parseInt(args[1]);
         }
         Logger logger =  LoggerFactory.getLogger(Main.class);
-        Instant target = Instant.now().plus(time, ChronoUnit.MILLIS);
-        var ex = new Inner().getData();
+        List<AtomicBoolean> buffer = IntStream
+                .range(0, threads)
+                .mapToObj(_id -> new AtomicBoolean(true)).toList();
+
+        Thread.ofVirtual()
+                .start(() -> {
+            LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(time));
+            for (var gate : buffer) {
+                gate.set(false);
+            }
+        });
+
 
         for (int i = 0; i < threads; i++) {
-            int finalI = i;
-            var thread = new Thread(() -> {
-                var val = ("[" + finalI + "]").repeat(3000);
-                while (Instant.now().isBefore(target)) {
+            final int finalI = i;
+            Thread.ofPlatform()
+                    .daemon(false)
+                    .name("penna-" + i).start(() -> {
+                while (buffer.get(finalI).get()) {
                     logger.info("hello");
-//                    logger.atInfo()
-//                            .setMessage("hello")
-////                            .setCause(ex)
-////                            .addKeyValue("key", val)
-//                            .log();
                 }
             });
-            thread.setName("penna-" + i);
-            thread.start();
         }
+
     }
 }
