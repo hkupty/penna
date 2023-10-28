f23c1da8dc52c9d24f1dd3ce5bd8bb5b2bd2562d
Henry Kupty <hkupty@gmail.com>
2023-10-28 17:51:50 +0200

    refactor: Use Info as default
    We should allow for per-test/block granularity. DEBUG/TRACE can be too
    verbose for tests, unnecessarily.

--- penna-dev/src/main/java/penna/dev/config/StandardConfigs.java
+++ penna-dev/src/main/java/penna/dev/config/StandardConfigs.java
@@ -7,5 +7,5 @@ import static penna.api.config.ConfigManager.ConfigItem;
 
 public final class StandardConfigs {
     private StandardConfigs() {}
-    public static ConfigItem DevOptimized = new ConfigItem.RootConfigItem(config -> new Config(Level.TRACE, config.fields(), config.exceptionHandling()));
+    public static ConfigItem DevOptimized = new ConfigItem.RootConfigItem(config -> new Config(Level.INFO, config.fields(), config.exceptionHandling()));
 }
