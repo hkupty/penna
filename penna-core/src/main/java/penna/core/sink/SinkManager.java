7c514ff642c71bb7d94243b76c4f87a6e197c21e
Henry Kupty <hkupty@gmail.com>
2023-11-14 00:07:24 +0100

    refactor: Simplify SinkManager

--- penna-core/src/main/java/penna/core/sink/SinkManager.java
+++ penna-core/src/main/java/penna/core/sink/SinkManager.java
@@ -2,14 +2,15 @@ package penna.core.sink;
 
 import penna.core.logger.PennaLogEventBuilder;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
 public final class SinkManager {
     private Supplier<Sink> factory = CoreSink::new;
-
-    private final Map<PennaLogEventBuilder, ProxySink> binding = new HashMap<>();
+    private final List<ProxySink> proxies = new ArrayList<>();
 
     public static final SinkManager Instance = new SinkManager();
 
@@ -17,18 +18,13 @@ public final class SinkManager {
 
     public void replace(Supplier<Sink> factory) {
         this.factory = factory;
-        for (ProxySink proxySink : binding.values()) {
-            proxySink.replaceImpl(factory.get());
-        }
+
+        proxies.forEach(proxy -> proxy.replaceImpl(factory.get()));
     }
 
-    public ProxySink get(PennaLogEventBuilder builder) {
-        ProxySink proxySink;
-        if ((proxySink = binding.get(builder)) == null) {
-            proxySink = new ProxySink(factory.get());
-            binding.put(builder, proxySink);
-        }
-
-        return proxySink;
+    public ProxySink get() {
+        var proxy = new ProxySink(factory.get());
+        proxies.add(proxy);
+        return proxy;
     }
 }
