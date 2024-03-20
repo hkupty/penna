43c1adc54bde47a3f2d80d861d475bb22aad07c0
Henry Kupty <hkupty@gmail.com>
2024-03-20 11:08:11 +0100

    refactor: Update sample for testing

--- sample/src/main/java/org/example/Main.java
+++ sample/src/main/java/org/example/Main.java
@@ -2,14 +2,32 @@ package org.example;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
-        MDC.put("Some", "value");
-        logger.debug("Testing!");
-        MDC.remove("Some");
+    public static void main(String[] args) throws InterruptedException {
+        var keepRunning = new AtomicBoolean(true);
+        var counter = new AtomicInteger();
+
+        while (keepRunning.get()) {
+            Thread.ofVirtual().name("Runner").start(() -> {
+                logger.debug("Still running running");
+                logger.atTrace()
+                        .addArgument(counter::get)
+                        .log("Counter is at {}");
+                var next = counter.incrementAndGet();
+                if (next >= 256) {
+                    keepRunning.set(false);
+                }
+            });
+            Thread.sleep(Duration.ofSeconds(2));
+        }
+
+
     }
 }
