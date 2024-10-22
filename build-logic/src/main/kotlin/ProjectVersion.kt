9ee8d46fcb17b03ea3ec329e54192e473a3a92d5
Henry Kupty <hkupty@gmail.com>
2024-10-22 12:39:02 +0200

    chore: Evaluate the version immediately

--- build-logic/src/main/kotlin/ProjectVersion.kt
+++ build-logic/src/main/kotlin/ProjectVersion.kt
@@ -1,30 +1,17 @@
-import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.TaskAction
-import org.gradle.kotlin.dsl.register
 import kotlin.io.path.readBytes
 
-abstract class ProjectVersion : DefaultTask() {
-    @TaskAction
-    fun action() {
-        try {
-            val version =
-                project.rootDir
-                    .toPath()
-                    .resolve("version")
-                    .readBytes()
-                    .toString(Charsets.UTF_8)
-            logger.info("Read $version")
-            project.version = version
-        } catch (ex: Exception) {
-            logger.warn("Unable to fetch version because of exception", ex)
-        }
-    }
-}
-
 class ProjectVersionPlugin : Plugin<Project> {
-    override fun apply(target: Project) {
-        target.tasks.register<ProjectVersion>("projectVersion")
+    override fun apply(project: Project) {
+        val version =
+            project.rootDir
+                .toPath()
+                .resolve("version")
+                .readBytes()
+                .toString(Charsets.UTF_8)
+        project.getLogger().info("Read $version")
+        project.version = version
+        project.setProperty("version", version)
     }
 }
