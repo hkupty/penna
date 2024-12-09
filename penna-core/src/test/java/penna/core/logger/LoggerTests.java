58af90e14209d2a9d2bb078200b2b3d0a700c85a
Henry Kupty <hkupty@gmail.com>
2024-12-09 11:56:56 +0100

    test: Add test to ensure we can log broken message

--- penna-core/src/test/java/penna/core/logger/LoggerTests.java
+++ penna-core/src/test/java/penna/core/logger/LoggerTests.java
@@ -145,4 +145,36 @@ class LoggerTests {
         fos.close();
     }
 
+    @Test
+    void we_can_log_a_broken_string_without_breaking_json_formatting() throws IOException {
+        var cache = new LoggerStorage();
+        PennaLogger logger = cache.getOrCreate("c.est.moi");
+
+        File testFile = File.createTempFile("broken-json", ".json");
+        FileOutputStream fos = new FileOutputStream(testFile);
+
+        TestContextPoolManager.replace(() -> new CoreSink(fos));
+
+        logger.atInfo().log("{");
+
+        Assertions.assertDoesNotThrow(() -> om.readValue(testFile, LogMessage.class), () -> {
+            try {
+                om.readValue(testFile, LogMessage.class);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                return ex.getMessage();
+            }
+
+            return "Unable to deserialize";
+        });
+
+        var logMessage = om.readValue(testFile, LogMessage.class);
+
+        Assertions.assertEquals("{", logMessage.message());
+
+        // Keep this line at the bottom, so we can inspect the file if the test breaks
+        testFile.deleteOnExit();
+        fos.close();
+    }
+
 }
