207ea618ff84fc4d4d4d8dd8954528c07019503c
Henry Kupty <hkupty@gmail.com>
2025-07-15 19:00:27 +0200

    chore: Migrate to .kts build file

---
+++ penna-yaml-config/build.gradle.kts
@@ -0,0 +1,134 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    `signing`
+    `pmd`
+
+    id("penna.build.projectVersion")
+}
+
+group = "com.hkupty.penna"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+pmd {
+    isConsoleOutput = true
+    toolVersion = "7.15.0"
+
+    sourceSets = listOf(java.sourceSets["main"])
+
+    ruleSets("category/java/performance.xml", "category/java/bestpractices.xml")
+}
+
+// Reproducible builds
+tasks.withType<AbstractArchiveTask>().configureEach {
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
+}
+
+// The API is an optional client-facing library and therefore it should not have gaps
+// in the documentation, nor fail on linting, so it is easier for consumers to understand
+// what it is doing under the hood.
+tasks.compileJava {
+    options.encoding = "UTF-8"
+    options.compilerArgs.addAll(
+        listOf(
+            "-Xlint:all",
+            // "-Xdoclint:all/public",
+            "-Werror",
+        ),
+    )
+}
+
+dependencies {
+    implementation(project(":penna-api"))
+    implementation(project(":penna-core"))
+    implementation(libs.slf4j)
+
+    // (optional) Jackson support
+    compileOnly(libs.jackson.core)
+    compileOnly(libs.jackson.databind)
+    compileOnly(libs.jackson.yaml)
+
+    // Annotations for better code readability
+    compileOnly(libs.jetbrains.annotations)
+
+    // (optional) SnakeYaml support
+    compileOnly(libs.snakeyaml.plain)
+    compileOnly(libs.snakeyaml.engine)
+
+    // Tests
+    testImplementation(libs.junit.api)
+    testImplementation(libs.junit.pioneer)
+    testRuntimeOnly(libs.junit.engine)
+    testRuntimeOnly(libs.jackson.core)
+    testRuntimeOnly(libs.jackson.databind)
+    testRuntimeOnly(libs.jackson.yaml)
+    testRuntimeOnly(libs.snakeyaml.engine)
+
+    testImplementation(libs.jackson.core)
+    testImplementation(libs.jackson.databind)
+    testImplementation(libs.jackson.yaml)
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Implementation-Version" to project.version,
+            "Implementation-Title" to project.name,
+        )
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("penna-version") {
+            groupId = "${project.group}"
+            artifactId = project.name
+            version = "${project.version}"
+
+            pom {
+                name = project.name
+                description = "An opinionated slf4j backend for structured logging"
+                url = "https://github.com/hkupty/penna"
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://github.com/hkupty/penna/blob/main/LICENSE"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "hkupty"
+                        name = "Henry John Kupty"
+                        email = "hkupty@gmail.com"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com/hkupty/penna"
+                    developerConnection = "scm:git:ssh://github.com/hkupty/penna"
+                    url = "https://github.com/hkupty/penna"
+                }
+            }
+        }
+    }
+}
+
+// signing {
+//     sign configurations.archives
+//     sign publishing.publications.mavenJava
+// }
